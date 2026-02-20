035208d0ece397e1ca40d07967677e77a6c1edc6
tom.fridental <tom.fridental@finaloop.com>
2026-02-20 22:37:51 +0200

    release: v1.0.1

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
 
-declare const __APP_VERSION__: string
+declare const __APP_VERSION__: string;
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
-    <span className="fixed bottom-1 left-1 text-[10px] text-gray-400 pointer-events-none select-none">
+    <span className="fixed bottom-2 left-2 text-md text-gray-400 pointer-events-none select-none">
       v{__APP_VERSION__}
     </span>
-  </StrictMode>,
-)
+  </StrictMode>
+);
